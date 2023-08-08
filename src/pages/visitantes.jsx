b2d01939709f4cb674fdcbfaa7648bd492a644fe
Pabs <villota.dev@gmail.com>
2023-08-08 16:40:44 -0500

    estructura data

--- src/pages/visitantes.jsx
+++ src/pages/visitantes.jsx
@@ -7,10 +7,14 @@ import { RestAPI } from "../utilities/restAPI"
 export default function Visitantes(props) {
   const restAPI = new RestAPI();
   const [date1, setDate1] = useState(null);
-  const [dataVisitors, setDataVisitors] = useState([]);
+  const [date2, setDate2] = useState(null);
+  const [dataLineChartVisitors, setDataLineChartVisitors] = useState({
+    labels: ["06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17"],
+    datasets: []
+  });
 
   const onChangeDate1 = (e) =>{
-    setDate1(e.value)
+    setDate1(e.value)            
     console.log(dateInFormat(e.value))
     let formattedDate = dateInFormat(e.value)
     let initDate = formattedDate.substring(0, 8) + 'T060000'
@@ -18,15 +22,31 @@ export default function Visitantes(props) {
 
     restAPI.getPeopleIn(initDate, finishDate)
     .then(data => {
-      setPeopleIn(data.events)
       let _peopleIn = data.events
       restAPI.getPeopleOut(initDate, finishDate)
         .then(data => {
-        setPeopleOut(data.events)
+        let _peopleOut = data.events
+        let visitorsTimeLine = getVisitors(mergeInTimeLine(_peopleIn, _peopleOut))
+        setDataLineChartVisitors(estructurarData(visitorsTimeLine))
+      })
+    })
+  }
+
+  const onChangeDate2 = (e) =>{
+    setDate2(e.value)
+    let formattedDate = dateInFormat(e.value)
+    let initDate = formattedDate.substring(0, 8) + 'T060000'
+    let finishDate = formattedDate.substring(0, 8) + 'T180000'
+
+    restAPI.getPeopleIn(initDate, finishDate)
+    .then(data => {
+      let _peopleIn = data.events
+      restAPI.getPeopleOut(initDate, finishDate)
+        .then(data => {
         let _peopleOut = data.events
 
         let visitorsTimeLine = getVisitors(mergeInTimeLine(_peopleIn, _peopleOut))
-        setVisitors(visitorsTimeLine)
+        setDataLineChartVisitors(estructurarData(visitorsTimeLine))
       })
     })
   }
@@ -62,7 +82,6 @@ export default function Visitantes(props) {
     }else{
       eventsTimeLine = [...eventsArray1, ...eventsArray2]
     }
-
     return eventsTimeLine
   }
   
@@ -80,42 +99,52 @@ export default function Visitantes(props) {
   const getVisitors = (eventsTimeline) => {
     //get the visitors timeline
     let visitorsTimeLine = [{hora: "06", visitors: 0}]
-    for (let i=0; i<eventsTimeLine.length; i++){
-      let hora = eventsTimeLine[i]["timestamp"].slice(9,11); // para tomar solo la hora
+    for (let i=0; i<eventsTimeline.length; i++){
+      let hora = eventsTimeline[i]["timestamp"].slice(9,11); // para tomar solo la hora
       let currentVisitorsCount = visitorsTimeLine[visitorsTimeLine.length-1]["visitors"] // última cuenta de visitantes
-      eventsTimeLine[i].type==="PeopleIn"?visitorsTimeLine.push({hora: hora, visitors: currentVisitorsCount++}):visitorsTimeLine.push({hora: hora, visitors: currentVisitorsCount--})
+      eventsTimeline[i].type==="PeopleIn"?visitorsTimeLine.push({hora: hora, visitors: currentVisitorsCount++}):visitorsTimeLine.push({hora: hora, visitors: currentVisitorsCount--})
     }
     return visitorsTimeLine
   }
 
 
   const parseDate = (dateString) =>{
+    // convert "YYYYMMDDThhmmss" to date
     const year = parseInt(dateString.substring(0, 4), 10);
     const month = parseInt(dateString.substring(4, 6), 10) - 1;
     const day = parseInt(dateString.substring(6, 8), 10);
     const hours = parseInt(dateString.substring(9, 11), 10);
     const minutes = parseInt(dateString.substring(11, 13), 10);
-    const seconds = parseInt(dateString.substring(13, 15), 10);
-  
+    const seconds = parseInt(dateString.substring(13, 15), 10);  
     return new Date(year, month, day, hours, minutes, seconds);
   }
 
-  const estructurarData = (registros) => {
+  const estructurarData = (registros, dateIndex) => {
+    // dateIndex gives information about what date source was changed
     const labelsValueObj = {}
     // {label1: valueLabel1, label2: valueLabel2 ....}
-    // Cuenta cuantos eventos han acontecido(eje y) por cada una de las etiquetas(eje x)
+    // Counts how many events have been happened (axis y) for each label(axis x)
     for (let i = 0; i < registros.length; i++) {
       let label = registros[i]["hora"]
       labelsValueObj[label] = labelsValueObj[label] ? labelsValueObj[label] + 1 : 1;
     }
 
-    //Split in 2 array labels and their respective values
+    //Split in 2 arrays labels and their respective values
     let arrayLabels = []
     let arrayValues = []
     for (const [label, value] of Object.entries(labelsValueObj)) {
       arrayLabels.push(label)
       arrayValues.push(value)
     }
+
+    let { datasets } = dataLineChartVisitors
+    datasets[dateIndex]= {
+      label: 'Día ' + (dateIndex + 1).toString(),
+      data: arrayValues,
+      fill: false,
+      borderColor: dateIndex===0?"blue":"red",
+      tension: 0.4
+    }    
 
     const data ={
       labels: arrayLabels,
@@ -170,14 +199,20 @@ export default function Visitantes(props) {
   return (
     <>
       <div className="row mt-3">
-        <div className="col-12">
+        <div className="col-6">
           <div className="card flex justify-content-center">
             <Calendar value={date1} onChange={(e) => onChangeDate1(e)} showIcon />
           </div>
         </div>
+        <div className="col-6">
+          <div className="card flex justify-content-center">
+            <Calendar value={date2} onChange={(e) => onChangeDate2(e)} showIcon />
+          </div>
+        </div>
+
         <div className="col-12">
           <div className="card">
-            <Chart type="line" data={estructurarData([])} options={options} />
+            <Chart type="line" data={dataLineChartVisitors} options={options} />
           </div>
         </div>
           
