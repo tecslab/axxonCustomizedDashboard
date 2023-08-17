1e890f463f23e51021f45603aee44aa27a829c4d
Pabs <villota.dev@gmail.com>
2023-08-16 22:27:53 -0500

    the last commit is this

--- src/pages/visitantes.jsx
+++ src/pages/visitantes.jsx
@@ -1,187 +1,263 @@
-let parametrosGlobales = {
-  //urlBack: "http://localhost:3000",
-  axxonOneServer: "127.0.0.1",
-  // axxonOneServer: "localhost",
-  axxonOnePort: "82",
-  prefix:"/",
-  user: "root",
-  password: "root"
-}
+import React, { useState, useEffect } from 'react'
+import { Chart } from 'primereact/chart';
+import { Calendar } from 'primereact/calendar';
+import { genDataBarStacked, genDataLine, dateInFormat } from "../parametrosGlobales"
+import { RestAPI } from "../utilities/restAPI"
 
+export default function Visitantes(props) {
+  const restAPI = new RestAPI();
+  const [date1, setDate1] = useState(null);
+  const [date2, setDate2] = useState(null);
+  const [dataLineChartVisitors, setDataLineChartVisitors] = useState({
+    labels: ["06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17"],
+    datasets: []
+  });
 
-// Common Functions
+  const onChangeDate1 = (e) =>{
+    setDate1(e.value)
+    let formattedDate = dateInFormat(e.value)
+    let initDate = formattedDate.substring(0, 8) + 'T060000'
+    let finishDate = formattedDate.substring(0, 8) + 'T180000'
 
-export const dateInFormat = (date) => {
-  const year = date.getFullYear().toString();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-
-  const formattedDate = `${year}${month}${day}T${hours}${minutes}${seconds}`;
-  return formattedDate;
-}
-
-
-// This function get a random color with some restrictions
-export const getRandomHexColor = () => {
-  const baseColor = 40; // 240 is a bright shade of grey, which gives a clear color
-  return '#' + randomHexColor() + randomHexColor() + randomHexColor() + baseColor.toString(16) + randomHexColor();
-}
-
-function randomHexColor() {
-  const randomColor = Math.floor(Math.random() * 16).toString(16);
-  return randomColor
-}
-
-export const genDataBarStacked = (registros, nombreColLabels, nombreColDS) => {
-  // Función para generar data para graficas de chart stacked
-  const listaLabels = [...new Set(registros.map(item => item[nombreColLabels]))].sort();
-  const listaDatasSets = [...new Set(registros.map(item => item[nombreColDS]))].sort()
-
-  let arrayLabelsDSs = []
-  // {label, datasets: {}}
-  for (let i = 0; i < listaLabels.length; i++) {
-    let registrosLabel = registros.filter(registro => registro[nombreColLabels]===listaLabels[i])
-
-    let DSxLabel = {};
-    for (let j = 0; j < registrosLabel.length; j++) {
-      let dataset = registrosLabel[j][nombreColDS];
-      DSxLabel[dataset] = DSxLabel[dataset] ? DSxLabel[dataset] + 1 : 1;
-    }
-    let objetoLabelDS = {label: listaLabels[i], datasets: DSxLabel}
-    arrayLabelsDSs.push(objetoLabelDS)
+    restAPI.getPeopleIn(initDate, finishDate)
+    .then(data => {
+      let _peopleIn = data.events
+      restAPI.getPeopleOut(initDate, finishDate)
+        .then(data => {
+        let _peopleOut = data.events
+        let visitorsTimeLine = getVisitors(mergeInTimeLine(_peopleIn, _peopleOut))
+        setDataLineChartVisitors(estructurarData(visitorsTimeLine, 1))
+      })
+    })
   }
 
-  let arrayDatasets = [] // Separa cada dataset en un array que está dentro de otro array
-  for (let i = 0; i <listaDatasSets.length; i++) {
-    let arrayDS = []
-    for (let j = 0; j < arrayLabelsDSs.length; j++){
-      let ocurrenciasDS = arrayLabelsDSs[j]["datasets"][listaDatasSets[i]] ? arrayLabelsDSs[j]["datasets"][listaDatasSets[i]] : 0
-      arrayDS.push(ocurrenciasDS)
-    }
-    arrayDatasets.push(arrayDS)
+  const onChangeDate2 = (e) =>{
+    setDate2(e.value)
+    let formattedDate = dateInFormat(e.value)
+    let initDate = formattedDate.substring(0, 8) + 'T060000'
+    let finishDate = formattedDate.substring(0, 8) + 'T180000'
+
+    restAPI.getPeopleIn(initDate, finishDate)
+    .then(data => {
+      let _peopleIn = data.events
+      restAPI.getPeopleOut(initDate, finishDate)
+        .then(data => {
+        let _peopleOut = data.events
+
+        let visitorsTimeLine = getVisitors(mergeInTimeLine(_peopleIn, _peopleOut))
+        setDataLineChartVisitors(estructurarData(visitorsTimeLine, 2))
+      })
+    })
   }
 
-  let datasets = []
+  const mergeInTimeLine = (eventsArray1, eventsArray2) =>{
+    // merge 2 array of events in one ordered Time line array
+    let eventsTimeLine = []
+    let indexEvents1 = 0
+    let indexEvents2 = 0
+    let condition = (indexEvents1<eventsArray1.length) && (indexEvents2<eventsArray2.length)
+    if (condition){
+      while (condition){
+        let nextEvent1 = eventsArray1[indexEvents1]
+        let nextEvent2 = eventsArray2[indexEvents2]
+        let inLower = isTheFirstTimestampLower(nextEvent1.timestamp, nextEvent2.timestamp)
 
-  for (let i = 0; i < listaDatasSets.length; i++){
-    let dataset = {
-      label: listaDatasSets[i],
-      backgroundColor: getRandomHexColor(),
-      borderColor: "black",
-      data: arrayDatasets[i]
-    }
-    datasets.push(dataset)
-  }
-
-  const dataBarStacked = {    
-    labels: listaLabels,
-    datasets: datasets
-  }
-
-  return dataBarStacked
-}
-
-export const genDataBar = (registros, nombreColLabels) => {
-  const labelsValueObj = {}
-  // {label1: valueLabel1, label2: valueLabel2 ....}
-  for (let i = 0; i < registros.length; i++) {
-    let label = registros[i][nombreColLabels];
-    labelsValueObj[label] = labelsValueObj[label] ? labelsValueObj[label] + 1 : 1;
-  }
-
-  let arrayLabels = [] 
-  let arrayValues= []
-  for (const [label, value] of Object.entries(labelsValueObj)) {
-    arrayLabels.push(label)
-    arrayValues.push(value)
-  }
-
-  const dataBarChart = {
-    labels: arrayLabels,
-    datasets: [
-      {
-        type: 'bar',
-        label: 'Ventas',
-        backgroundColor: getRandomHexColor(),
-        data: arrayValues
+        if (inLower){
+          eventsTimeLine.push(nextEvent1)
+          indexEvents1 ++
+          if (indexEvents1>=eventsArray1.length){
+            condition = false
+            eventsTimeLine.push(...eventsArray2.slice(indexEvents2))
+          }
+        }else{
+          eventsTimeLine.push(nextEvent2)
+          indexEvents2 ++
+          if (indexEvents2>=eventsArray2.length){
+            condition = false
+            eventsTimeLine.push(...eventsArray1.slice(indexEvents1))
+          }
+        }
       }
-    ]
+    }else{
+      eventsTimeLine = [...eventsArray1, ...eventsArray2]
+    }
+    return eventsTimeLine
+  }
+  
+  const isTheFirstTimestampLower = (timestamp1, timestamp2) =>{
+    // answer if the first of 2 events has the lower timestamp
+    const date1 = parseDate(timestamp1);
+    const date2 = parseDate(timestamp2);
+  
+    if (isNaN(date1) || isNaN(date2)) {
+      return "Invalid date format";
+    } 
+    return date1 < date2 ? true : false;
+  }
+
+  const getVisitors = (eventsTimeline) => {
+    console.log(eventsTimeline)
+    //get the visitors timeline
+    let visitorsTimeLine = [{hora: "06", visitors: 0}]
+    for (let i=0; i<eventsTimeline.length; i++){
+      let hora = eventsTimeline[i]["timestamp"].slice(9,11); // para tomar solo la hora
+      let currentVisitorsCount = visitorsTimeLine[visitorsTimeLine.length-1]["visitors"] // última cuenta de visitantes
+      //console.log(currentVisitorsCount)
+      eventsTimeline[i].type==="PeopleIn"?currentVisitorsCount++:currentVisitorsCount--
+      visitorsTimeLine.push({hora: hora, visitors: currentVisitorsCount})
+    }
+    return visitorsTimeLine
+  }
+
+  const parseDate = (dateString) =>{
+    // convert "YYYYMMDDThhmmss" to date
+    const year = parseInt(dateString.substring(0, 4), 10);
+    const month = parseInt(dateString.substring(4, 6), 10) - 1;
+    const day = parseInt(dateString.substring(6, 8), 10);
+    const hours = parseInt(dateString.substring(9, 11), 10);
+    const minutes = parseInt(dateString.substring(11, 13), 10);
+    const seconds = parseInt(dateString.substring(13, 15), 10);  
+    return new Date(year, month, day, hours, minutes, seconds);
+  }
+
+  const estructurarData = (visitorsTimeLine, dateIndex) => {
+    //console.log(visitorsTimeLine)
+    // dateIndex gives information about what date source was changed
+    
+    // out: Max visitors registered in an interval. If there are no registers in some interval it keeps the last register.
+    // [{interval: "06", visitors: 1}, {interval: "07", visitors: 3}, ... ]
+    const intervals = ['06H00', '07H00', '08H00', '09H00', '10H00', '11H00', '12H00', '13H00', '14H00', '15H00', '16H00', '17H00', '18H00']
+    // {label1: valueLabel1, label2: valueLabel2 ....}
+    let lastVisitors = visitorsTimeLine[0].visitors // it is needed to give the count of visitors when there are not registers from this interval
+    let intervalVisitorsData = []
+    //console.log(visitorsTimeLine)
+    for (let interval of intervals){
+      // filter all register in each interval
+      //let maxVisitorsObject = null;
+      let maxVisitorsValue = 0
+      let registrosIntervalo = visitorsTimeLine.filter(registro => registro.hora === interval.substring(0, 2))
+      
+      if (registrosIntervalo.length === 0) {
+        //maxVisitorsObject = { interval: interval, visitors: lastVisitors }
+        maxVisitorsValue = lastVisitors
+      }else{
+        lastVisitors = registrosIntervalo[registrosIntervalo.length-1].visitors
+        let maxVisitors = -Infinity;
+        
+        for (const registro of registrosIntervalo) {
+          const visitors = registro.visitors;
+          if (visitors > maxVisitors) {
+            maxVisitors = visitors;
+          }
+        }
+        // maxVisitorsObject = { interval: interval, visitors: maxVisitors };
+        maxVisitorsValue = maxVisitors
+      }
+      //intervalVisitorsData.push(maxVisitorsObject)
+      intervalVisitorsData.push(maxVisitorsValue)
+    }
+
+    let { datasets } = dataLineChartVisitors
+    datasets[dateIndex-1]= {
+      label: 'Día ' + dateIndex.toString(),
+      data: intervalVisitorsData,
+      fill: false,
+      borderColor: dateIndex-1===0?"blue":"red",
+      tension: 0.4
+    }
+
+    /*if (datasets[0]===undefined){
+      datasets[0] = 
+    }*/
+
+    const data ={
+      labels: intervals,
+      datasets: datasets
+    }
+
+    console.log(data)
+
+    return data
+  }
+
+  const options = {
+    maintainAspectRatio: false,
+    aspectRatio: 0.6,
+    plugins: {
+      legend: {
+        labels: {
+          color: "black"
+        }
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: "black"
+        },
+        grid: {
+          color: "black"
+        }
+      },
+      y: {
+        ticks: {
+          color: "black"
+        },
+        grid: {
+          color: "black"
+        }
+      }
+    }
   };
 
-  return dataBarChart
+  useEffect(() => {
+    let today = new Date()
+    console.log(today)
+    setDate1(today)
+    let formattedDate = dateInFormat(today)
+    let initDate = formattedDate.substring(0, 8) + 'T060000'
+    let finishDate = formattedDate.substring(0, 8) + 'T180000'
+
+    restAPI.getPeopleIn(initDate, finishDate)
+    .then(data => {
+      console.log(data)
+      let _peopleIn = data.events
+      restAPI.getPeopleOut(initDate, finishDate)
+        .then(data => {
+        let _peopleOut = data.events
+        let visitorsTimeLine = getVisitors(mergeInTimeLine(_peopleIn, _peopleOut))
+        setDataLineChartVisitors(estructurarData(visitorsTimeLine, 1))
+      })
+    })
+  },[])
+
+  
+  return (
+    <>
+      <div className="row mt-3">
+      <div className="col-12">
+          <div className="card flex justify-content-center">
+            <h1>Visitantes máximos por hora</h1>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="card flex justify-content-center">
+            <Calendar value={date1} onChange={(e) => onChangeDate1(e)} showIcon />
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="card flex justify-content-center">
+            <Calendar value={date2} onChange={(e) => onChangeDate2(e)} showIcon />
+          </div>
+        </div>
+
+        <div className="col-12">
+          <div className="card">
+            <Chart type="line" data={dataLineChartVisitors} options={options} />
+          </div>
+        </div>
+          
+      </div>
+    </>
+  )
 }
-
-export const genDataLine = (registros, nombreColLabels) => {
-  const labelsValueObj = {}
-  // {label1: valueLabel1, label2: valueLabel2 ....}
-  // Counts how many events have been happened (axis y) for each label(axis x)
-  for (let i = 0; i < registros.length; i++) {
-    let label = registros[i][nombreColLabels];
-    labelsValueObj[label] = labelsValueObj[label] ? labelsValueObj[label] + 1 : 1;
-  }
-
-  //Split in 2 array labels and their respective values
-  let arrayLabels = []
-  let arrayValues = []
-  for (const [label, value] of Object.entries(labelsValueObj)) {
-    arrayLabels.push(label)
-    arrayValues.push(value)
-  }
-
-  const dataLineChart = {
-    labels: arrayLabels.length===3?["Enero", "Febrero", "Marzo"]:arrayLabels,
-    datasets: [
-      {
-        label: 'Ventas',
-        data: arrayValues,
-        fill: false,
-        tension: 0.4,
-        borderColor: getRandomHexColor()
-      }
-    ]
-  };
-
-  return dataLineChart
-}
-
-export const optionsBarChart = {
-  maintainAspectRatio: false,
-  aspectRatio: 0.8,
-  plugins: {
-    tooltips: {
-      mode: 'index',
-      intersect: false
-    },
-    legend: {
-      labels: {
-        color: "black"
-      }
-    }
-  },
-  scales: {
-    x: {
-      stacked: true,
-      ticks: {
-        color: "black"
-      },
-      grid: {
-        color: "#0f0d142e"
-      }
-    },
-    y: {
-      stacked: true,
-      ticks: {
-        color: "black"
-      },
-      grid: {
-        color: "#0f0d142e"
-      }
-    }
-  }
-};
-
-
-export default parametrosGlobales
