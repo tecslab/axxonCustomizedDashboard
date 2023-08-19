44ed1cb5d0e8cf2f25472a8e757fd16158cddb9b
Pabs <villota.dev@gmail.com>
2023-08-19 18:24:10 -0500

    fix time bug

--- src/utilities/restAPI.js
+++ src/utilities/restAPI.js
@@ -7,7 +7,7 @@ export class RestAPI {
   getPeopleIn({initDate, finishDate}){
 
     let baseURI = "http://" + axxonOneServer + ":" + axxonOnePort + prefix
-    let uriPeopleIn = baseURI + 'archive/events/detectors' + vEntranceCamera + initDate + "/" + finishDate + "?type=PeopleIn&limit=600"
+    let uriPeopleIn = baseURI + 'archive/events/detectors' + vEntranceCamera + initDate + "/" + finishDate + "?type=PeopleIn&limit=800"
     console.log(uriPeopleIn)
     const headers = new Headers();
     headers.set('Authorization', 'Basic ' + btoa(user + ':' + password));
@@ -22,7 +22,7 @@ export class RestAPI {
 
   getPeopleOut({initDate, finishDate}){
     let baseURI = "http://" + axxonOneServer + ":" + axxonOnePort + prefix
-    let uriPeopleOut = baseURI + 'archive/events/detectors' + vEntranceCamera + initDate + "/" + finishDate + "?type=PeopleOut&limit=600"
+    let uriPeopleOut = baseURI + 'archive/events/detectors' + vEntranceCamera + initDate + "/" + finishDate + "?type=PeopleOut&limit=800"
     const headers = new Headers();
     headers.set('Authorization', 'Basic ' + btoa(user + ':' + password));
     console.log(uriPeopleOut)
@@ -36,7 +36,7 @@ export class RestAPI {
 
   getFaces({initDate, finishDate}){
     let baseURI = "http://" + axxonOneServer + ":" + axxonOnePort + prefix
-    let uriFaceAppeared = baseURI + 'archive/events/detectors/'+ initDate + "/" + finishDate + "?type=faceAppeared&limit=600"
+    let uriFaceAppeared = baseURI + 'archive/events/detectors/'+ initDate + "/" + finishDate + "?type=faceAppeared&limit=800"
     const headers = new Headers();
     headers.set('Authorization', 'Basic ' + btoa(user + ':' + password));
     return fetch(uriFaceAppeared, {headers})
